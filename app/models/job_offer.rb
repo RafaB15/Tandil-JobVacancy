d2785b73ce1515f1e2d97e52547d9b13f26db203
Mint <mint@mint-VirtualBox.(none)>
2013-07-25 20:56:48 +0200

    Better implementation of property owner

--- app/models/job_offer.rb
+++ app/models/job_offer.rb
@@ -1,17 +1,12 @@
 class JobOffer
-  include DataMapper::Resource
+	include DataMapper::Resource
 
-  # property <name>, <type>
-  property :id, Serial
-  property :title, String
-  property :location, String
-  property :description, String
-  belongs_to :user
+	# property <name>, <type>
+	property :id, Serial
+	property :title, String
+	property :location, String
+	property :description, String
+	belongs_to :owner, :model => 'User'
 
 	validates_presence_of :title
-
-	def owner
-		user
-	end
-
 end
