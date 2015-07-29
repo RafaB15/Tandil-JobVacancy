ecd15afc10a196ac5fe23675e598214af193b179
NicoPaez <nicopaez@gmail.com>
2015-07-29 09:56:25 -0300

    Initial status improved

--- app/controllers/health.rb
+++ app/controllers/health.rb
@@ -21,7 +21,28 @@ JobVacancy::App.controllers :health do
     user = User.create(:email => 'offerer@test.com',
                    :name => 'Offerer', 
                    :password => "Passw0rd!")
+    JobOffer.create(:title => 'Java programmer',
+                    :description => 'Spring experience required',
+                    :location => 'Cordoba')
+    JobOffer.create(:title => 'Web programmer',
+                    :description => 'HTML5 experience required',
+                    :location => 'Rosario')
     'ok'
   end
+
+
+    @job_offer = JobOffer.new(params[:job_offer])
+    @job_offer.owner = current_user
+    if @job_offer.save
+      if params['create_and_twit']
+        TwitterClient.publish(@job_offer)
+      end
+      flash[:success] = 'Offer created'
+      redirect '/job_offers/my'
+    else
+      flash.now[:error] = 'Title is mandatory'
+      render 'job_offers/new'
+    end  
+
 
 end
