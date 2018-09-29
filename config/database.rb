f2ee229f95cdb951e8a00e8e14d337e936a57a8e
nicopaez <nicopaez@computer.org>
2018-09-29 11:56:14 -0300

    Fix in the DB config for test

--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,7 @@ Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect("postgres://jobvacancy:jobvacancy@localhost/jobvacancy_development", :loggers => [logger])
-  when :test        then Sequel.connect("postgres://jobvacancy:jobvacancy@localhost/jobvacancy_test", :loggers => [logger])
+  when :test        then Sequel.connect("postgres://jobvacancy:jobvacancy@#{ENV['DB_HOST']||'localhost'}/jobvacancy_test", :loggers => [logger])
   when :staging     then Sequel.connect(ENV['DATABASE_URL'], loggers: [logger])
   when :production  then Sequel.connect(ENV['DATABASE_URL'], loggers: [logger])
 end
