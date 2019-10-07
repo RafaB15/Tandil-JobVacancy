32d0d5ea6df8fdb48de86135b053499ee4a50a91
NicoPaez <nicopaez@gmail.com>
2019-10-07 07:55:13 -0300

    Mailer configuration updated

--- app/app.rb
+++ app/app.rb
@@ -6,8 +6,21 @@ module JobVacancy
 
     enable :sessions
 
-    set :delivery_method, file: {
-      location: "#{Padrino.root}/tmp/emails"
-    }
+    Padrino.configure :development, :test do
+      set :delivery_method, file: {
+        location: "#{Padrino.root}/tmp/emails"
+      }
+    end
+
+    Padrino.configure :staging, :production do
+      set :delivery_method, smtp: {
+        address: ENV['SMTP_ADDRESS'],
+        port: ENV['SMTP_PORT'],
+        user_name: ENV['SMTP_USER'],
+        password: ENV['SMTP_PASS'],
+        authentication: :plain,
+        enable_starttls_auto: true
+      }
+    end
   end
 end
