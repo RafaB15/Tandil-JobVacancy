88cd2f21093dc326f07418bf9684886dc79db58f
NicoPaez <nicopaez@computer.org>
2023-03-07 10:43:43 -0300

    Seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,30 @@
 require_relative '../models/user'
+require_relative '../models/job_offer'
 
 user_repository = UserRepository.new
 unless user_repository.all.count.positive?
-  user = User.new(email: 'offerer@test.com',
+  test_user = User.new(email: 'offerer@test.com',
                   name: 'Offerer',
                   password: 'Passw0rd!')
 
-  user_repository.save user
+  user_repository.save test_user
 end
+
+test_user = user_repository.find_by_email 'offerer@test.com'
+
+offer_repository = JobOfferRepository.new
+ruby_offer = JobOffer.new(title: 'Ruby Dev', 
+  location: 'Baires, Argentina',
+  description: 'Remote',
+  is_active: true)
+ruby_offer.owner = test_user
+offer_repository.save ruby_offer
+
+node_offer = JobOffer.new(title: 'Node Dev', 
+  location: 'Rio, Brazil',
+  description: 'Remote',
+  is_active: true)
+node_offer.owner = test_user
+offer_repository.save node_offer
+
+
